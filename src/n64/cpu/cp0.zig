d52d00bfd570868f76470433bdc0befb30b6715d
James Lippitt <711298+jlippitt@users.noreply.github.com>
2025-11-04 13:35:28 +0000

    Implement config register writes

--- src/n64/cpu/cp0.zig
+++ src/n64/cpu/cp0.zig
@@ -3,10 +3,14 @@ const cpu = @import("../cpu.zig");
 const util = @import("../util.zig");
 
 const reg_names: [32][]const u8 = .{
-    "ZERO", "AT", "V0", "V1", "A0", "A1", "A2", "A3",
-    "T0",   "T1", "T2", "T3", "T4", "T5", "T6", "T7",
-    "S0",   "S1", "S2", "S3", "S4", "S5", "S6", "S7",
-    "T8",   "T9", "K0", "K1", "GP", "SP", "FP", "RA",
+    "Index",    "Random",   "EntryLo0",    "EntryLo1",
+    "Context",  "PageMask", "Wired",       "R7",
+    "BadVAddr", "Count",    "EntryHi",     "Compare",
+    "Status",   "Cause",    "EPC",         "PRId",
+    "Config",   "LLAddr",   "WatchLo",     "WatchHi",
+    "XContext", "R21",      "R22",         "R23",
+    "R24",      "R25",      "ParityError", "CacheError",
+    "TagLo",    "TagHi",    "ErrorEPC",    "R31",
 };
 
 var _status: packed struct(u32) {
@@ -18,7 +22,7 @@ var _status: packed struct(u32) {
     sx: bool = false,
     kx: bool = false,
     im: u8 = 0,
-    ds: u9 = 0x40,
+    ds: u9 = 0b0_0100_0000,
     re: bool = false,
     fr: bool = false,
     rp: bool = false,
@@ -26,6 +30,17 @@ var _status: packed struct(u32) {
     cu1: bool = false,
     cu2: bool = false,
     cu3: bool = false,
+} = .{};
+
+var _config: packed struct(u32) {
+    k0: u3 = 0,
+    cu: bool = false,
+    _0: u11 = 0b110_0100_0110,
+    be: bool = true,
+    _1: u8 = 0b0000_0110,
+    ep: u4 = 0,
+    ec: u3 = 0b111,
+    _2: u1 = 0,
 } = .{};
 
 pub fn init() void {}
@@ -50,6 +65,19 @@ pub fn set(reg: u5, value: u64) void {
             }
 
             std.log.debug("  Status: {any}", .{_status});
+        },
+        16 => {
+            util.writeWithMask(u32, @ptrCast(&_config), util.bitTruncate(u32, value), 0x0f00_800f);
+
+            if (!_config.be) {
+                std.log.warn("Unsupported: Little-endian mode", .{});
+            }
+
+            if (_config.ep != 0) {
+                std.log.warn("Unsupported: Non-default data transfer patterns", .{});
+            }
+
+            std.log.debug("  Config: {any}", .{_config});
         },
         else => std.debug.panic("Unimplemented: CP0 register write {} <= {X:016}", .{ reg, value }),
     }
